039eb6f856aeb2118587781b1654a5ad590e1189
mohammadjaloudi <141223023+mohammadjaloudi@users.noreply.github.com>
2024-07-11 06:08:54 +0300

    Update catchTheBall.py

--- Games/catchTheBall.py
+++ Games/catchTheBall.py
@@ -1,84 +1,101 @@
 import turtle
-from random import randint
+from random import randint, uniform
+
+def random_number():
+    uniform_random = uniform(0, 1)
+    skewed_random = uniform_random ** 3
+    scaled_random = 0.001 + skewed_random * (0.05 - 0.001)
+    return scaled_random
+
+def go_to_left():
+    x = paddle.xcor()
+    x -= 15
+    if x > -270:
+        paddle.setx(x)
+
+def go_to_right():
+    x = paddle.xcor()
+    x += 15
+    if x < 270:
+        paddle.setx(x)
+
+def direction():
+    y = random_number()
+    left_or_right = randint(0, 1)
+    x = 0.5
+    if left_or_right == 0:
+        x = -0.5
+    
+    return x, y
+
+x_axis, y_axis = direction()
+speed = 5 + y_axis
+
+def moving_game():
+    global speed, x_axis, y_axis
+    
+    ball.sety(ball.ycor() - speed)
+    ball.setx(ball.xcor() + x_axis)
+    
+    if ball.xcor() > 270 or ball.xcor() < -270:
+        x_axis *= -1
+    
+    if ball.ycor() < -320:
+        x_ball = randint(-270, 270)
+        ball.goto(x_ball, 270)
+        scoring_game(-1)
+    
+    if ball.ycor() < -290 and ball.ycor() > -310 and paddle.xcor() - 50 < ball.xcor() < paddle.xcor() + 50:
+        scoring_game(1)
+        speed += y_axis
+        x_ball = randint(-270, 270)
+        ball.goto(x_ball, 270)
+        x_axis, y_axis = direction()
+    
+    sc.ontimer(moving_game, 10)
+
+        
+def scoring_game(n):
+    global score
+    score += n
+    scoring.clear()
+    scoring.write(f"Score: {score}", align="center", font={"Arial", 24, "normal"})
+
 
 sc = turtle.Screen()
-sc.title("Catch the ball game")
-sc.setup(width=600, height=600)
-sc.bgcolor("white")
+sc.setup(700, 700)
+sc.bgcolor("black")
+sc.title("Catch the ball")
 
-boreder = turtle.Turtle()
-boreder.speed(0)
-boreder.color("black")
-boreder.penup()
-boreder.setposition(-290, -290)
-boreder.pendown()
-boreder.pensize(3)
-for i in range(4):
-     boreder.forward(580)
-     boreder.left(90)
-boreder.hideturtle()
+scoring = turtle.Turtle()
+scoring.speed(0)
+scoring.color("white")
+scoring.penup()
+scoring.goto(0, 300)
+score = 0
+scoring.write(f"Score: {score}", align="center", font={"Arial", 24, "normal"})
+scoring.hideturtle()
 
 paddle = turtle.Turtle()
 paddle.speed(0)
 paddle.shape("square")
-paddle.color("orange")
 paddle.shapesize(stretch_wid=1, stretch_len=5)
+paddle.color("white")
 paddle.penup()
-paddle.goto(0, -250)
+paddle.goto(0, -300)
 
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("circle")
 ball.color("red")
 ball.penup()
-ball.goto(randint(-250, 250), 250)
-
-ball_speed = 1
-
-score = 0
-
-score_display = turtle.Turtle()
-score_display.speed(0)
-score_display.color("black")
-score_display.penup()
-score_display.hideturtle()
-score_display.goto(0, 260)
-score_display.write(f"Score: {score}", align="center", font={"Courier", 24, "nomral"})
-
-def scoring():
-    score_display.clear()
-    score_display.write(f"Score: {score}", align="center", font={"Courier", 24, "nomral"})
-
-def left_paddle():
-    x = paddle.xcor()
-    x -= 20
-    if x > -250:
-        paddle.setx(x)
-
-def right_paddle():
-    x = paddle.xcor()
-    x += 20
-    if x < 250:
-        paddle.setx(x)
+x_ball = randint(-270, 270)
+ball.goto(x_ball, 270)
 
 sc.listen()
-sc.onkeypress(right_paddle, "Right")
-sc.onkeypress(left_paddle, "Left")
+sc.onkeypress(go_to_left, "Left")
+sc.onkeypress(go_to_right, "Right")
 
-def game():
-    global score 
-    ball.sety(ball.ycor() - ball_speed)
-    
-    if ball.ycor() < -290:
-        ball.goto(randint(-250, 250), 250)
-    
-    if ball.ycor() <= -240 and ball.ycor() >= -260 and paddle.xcor() - 50 < ball.xcor() < paddle.xcor() + 50:
-        ball.goto(randint(-255, 255), 255)
-        score += 1
-        scoring()
-    
-    sc.ontimer(game, 1)
-        
+moving_game()
 
-game()
 sc.mainloop()
