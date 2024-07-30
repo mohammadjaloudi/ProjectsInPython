10186014e232bbb5f8ad5f7b1d1eca0e6d758d8c
mohammadjaloudi <141223023+mohammadjaloudi@users.noreply.github.com>
2024-07-30 18:55:25 +0300

    Update ToDoList.py

--- MiniPythonProjects/ToDoList.py
+++ MiniPythonProjects/ToDoList.py
@@ -1,33 +1,47 @@
 class ToDoList:
     def __init__(self):
-        self.tasks = []
+        self.tasks = {}
     
-    def add_task(self, task) -> None:
+    def add_task(self, task, time) -> None:
         if task in self.tasks:
-            print("Task is already exist.")
+            print("Task already exists.")
             return None
         
-        self.tasks.append(task)
+        try:
+            th, tm = time.split(":")
+            task_mins = int(th) * 60 + int(tm)
+        except ValueError:
+            print("Invalid time format. Please use HH:MM format.")
+            return None
+        
+        for _, time_d in self.tasks.items():
+            th, tm = time_d.split(":")
+            curr_mins = int(th) * 60 + int(tm)
+            if curr_mins <= task_mins <= curr_mins + 60:
+                print("Time is unavailable")
+                return None
+        
+        self.tasks[task] = time
         print("Task has been added successfully.")
         return None
     
-    def finish_task(self, pos) -> None:
-        if pos > len(self.tasks):
+    def finish_task(self, task_name) -> None:
+        if task_name not in self.tasks:
             print("Task doesn't exist.")
-            print(f"There exist {len(self.tasks)} tasks")
+            print("You can display the tasks if you want to know what are the available")
             return None
         
-        self.tasks.pop(pos - 1)
+        del self.tasks[task_name]
         print("Task done successfully! Good job :)")
         return None
     
     def view_tasks(self) -> None:
         if not self.tasks:
-            print("No tasks exists :(")
+            print("No tasks exist :(")
             return None
         
-        for number, task in enumerate(self.tasks, start=1):
-            print(f"Task {number} is: {task}")
+        for number, (task, time) in enumerate(self.tasks.items(), start=1):
+            print(f"Task {number} is: {task} on time {time}")
         return None
 
 def main():
@@ -46,14 +60,16 @@ def main():
             match option:
                 case 1:
                     task = input("Enter the task that you want to add: ").title()
-                    to_do_list.add_task(task)
+                    time = input("Enter the time of the task (HH:MM): ")
+                    to_do_list.add_task(task, time)
                 case 2:
-                    task = input("Enter the number of the task that you've finished: ").title()
+                    task_number = input("Enter the number of the task that you've finished: ")
                     try:
-                        task = int(task)
+                        task_number = int(task_number)
+                        task = list(to_do_list.tasks.keys())[task_number - 1]
                         to_do_list.finish_task(task)
-                    except ValueError:
-                        print("Inalid number of a task!")
+                    except (ValueError, IndexError):
+                        print("Invalid task number!")
                 case 3:
                     to_do_list.view_tasks()
                 case 4:
